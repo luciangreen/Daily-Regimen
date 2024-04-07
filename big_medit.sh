ba923ee69cb3bdb7951f855b8ccba89e2bba379c
Lucian Green <luciangreen@lucianacademy.com>
2024-04-08 00:17:09 +1000

    Update big_medit.sh

--- big_medit.sh
+++ big_medit.sh
@@ -1,9 +1,9 @@
-# Comment the start of ./cgpt_combophil3_10.sh with '#' if you don't want to use ChatGPT as an accreditation tool in each dimension
+# Comment the start of ./cgpt_combophil3_160.sh with '#' if you don't want to use ChatGPT as an accreditation tool in each dimension
 ./cgpt_combophil3_160.sh
 ./texttobr2_square.sh
 ./d.sh
 ./day2.sh
-# ./a15_meditators.sh prepares an apartment in each dimension for meditators by helping those 15 of those around them
+# ./a15_meditators.sh prepares an apartment in each dimension for meditators by helping 15 of those around them
 ./a15_meditators.sh
 ./daily_medit.sh
 ./group_meditation.sh
