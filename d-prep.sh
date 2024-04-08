eda5add68d6f4dc3d0cc22417c62ca71fb4ab9db
Lucian Green <luciangreen@lucianacademy.com>
2024-04-08 19:01:29 +1000

    Sped up d.sh

---
+++ d-prep.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+# One-off
+swipl --goal=main --stand_alone=true -o t2b4 -c text_to_breasonings4.pl 
+
+# Each week
+cd ../Philosophy
+
+./cat_arg_files.sh
+./cat_alg_files.sh
+
+cd ../Text-to-Breasonings
+
+cp ../Lucian-Academy/Books1/algs/lgalgs_a.txt file.txt
+./t2b4
+rm a.pl
+mv a a1
+rm b.pl
+mv b b1
+rm c.pl
+mv c c1
+
+cp ../Lucian-Academy/Books1/args/lgtext_a.txt file.txt
+./t2b4
+rm a.pl
+mv a a2
+rm b.pl
+mv b b2
+rm c.pl
+mv c c2
