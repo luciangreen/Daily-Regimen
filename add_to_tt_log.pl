4d5ecbc1a901acaaec43f2ea34670ebe9e1381cb
Lucian Green <luciangreen@lucianacademy.com>
2024-04-09 21:42:23 +1000

    Sped up big_medit

--- add_to_tt_log.pl
+++ add_to_tt_log.pl
@@ -4,11 +4,14 @@
 main :-
 
 catch((open_file_s("tt_log.txt",File_term),
-(append(_,[[n=N1,_Pres_D,_Pres_M,_Pres_Y,Fut_D,Fut_M,Fut_Y]],File_term)->true;(N1=0,Fut_D=0,Fut_M=10,Fut_Y=5689)),
+(append(_,[[n=N1,Pres_D,Pres_M,Pres_Y,Fut_D,Fut_M,Fut_Y]],File_term)->true;(N1=0,Fut_D=1,Fut_M=10,Fut_Y=5689)),
 N2 is N1+1,
-Fut_D1 is (Fut_D+1) mod 16,
 
 get_time(TS),stamp_date_time(TS,date(Year,Month,Day,_,_,_,_,_,_),local),
+
+([Pres_D,Pres_M,Pres_Y]=[Day,Month,Year]->D=0;D=1),
+Fut_D0 is ((Fut_D+D) mod 17),
+(Fut_D0=0->Fut_D1=1;Fut_D0=Fut_D1),
 
 append(File_term,[[n=N2,Day,Month,Year,Fut_D1,Fut_M,Fut_Y]],File_term1),   term_to_atom(File_term1,String1),
 	save_file_s("tt_log.txt",String1)),Err,handle_error(Err)),halt.
---
+++ big_medit2.pl
@@ -0,0 +1,64 @@
+% big_medit2.pl
+
+:-include('../listprologinterpreter/listprolog.pl').
+:-include('../Text-to-Breasonings/meditatorsanddoctors.pl').
+:-include('../Text-to-Breasonings/meditationnoreplace2.pl').
+
+main:-catch(main2,Err,handle_error(Err)),halt.
+handle_error(_Err):-
+  halt(1).
+main :- halt(1).
+
+main2 :-
+((exists_file('../Text-to-Breasonings/b1'),
+exists_file('../Text-to-Breasonings/b2'),
+exists_file('../Text-to-Breasonings/c1'),
+exists_file('../Text-to-Breasonings/c2'))->
+true;
+shell1_s("./d-prep.sh")),
+working_directory1(WD,WD),
+working_directory1(_,"../Text-to-Breasonings"),
+shell1_s("./bc12.sh"),
+working_directory1(_,WD),
+shell1_s("./add_to_tt_log.sh"),
+
+meditators(A),meditators2(B),length(A,AL),length(B,BL),CL is AL+BL,
+
+meditation1(Utterances1,Utterances2,Immortality),
+
+length(Utterances1,UL1),
+length(Utterances2,UL2),
+
+length(Immortality,J2),
+
+% breasonings:
+
+N1 is 	2*108*250 +				% meditation
+		16000 + 4 +				% texttobr2_square
+		160 + 					% 160 combophil br
+		15*3*4*16000 + 			% d.sh
+		3*10*4*16000 + 			% day2.sh 
+								% with medit, tt, medic frozen age, 
+								% hq thought
+		250*CL +				% a15_meditators.sh prepares an apartment 
+								% in each dimension for meditators by 
+								% helping 15 of those around them
+		3*10*(UL1+UL2)*250 +	% daily_medit.sh
+		3*10*J2*250,
+
+GM is 	250*CL,					% group_meditation.sh
+
+C is	GM +					
+		16000+					% time_hop.sh
+		2*108*16000 +			% meditation
+		GM,
+
+T is	N1+
+		C+ % Going to present
+		N1+
+		C+ % Going to 5689
+		N1,
+
+N2 is (T mod 16000)+1,
+
+time(texttobr2_1(N2)),!.
